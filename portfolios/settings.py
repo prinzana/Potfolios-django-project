b4da86fc5c114130bb830c11c36aefb83eabc5c9
Zana <prinzana@gmail.com>
2021-06-15 12:44:52 +0100

    Jobs app up and running

--- portfolios/settings.py
+++ portfolios/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,8 +80,15 @@ WSGI_APPLICATION = 'portfolios.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        ##'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        ##'NAME': BASE_DIR / 'db.sqlite3',
+         ##'NAME' : os.path.join( BASE_DIR / 'db.sqlite3'),
+         'NAME': 'portfoliosdb',
+         'USER': 'postgres',
+         'PASSWORD':'1325196Pge',
+         'HOST': 'localhost',
+         'PORT': '5432',
     }
 }
 
@@ -120,7 +131,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT =  BASE_DIR / 'db.sqlite3',
+MEDIA_ROOT =  os.path.join( BASE_DIR / 'media')
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
