5e532d1efbab5a92391537ce4f74adda022b6be2
Zana <prinzana@gmail.com>
2021-07-03 14:18:39 +0100

    Finishing and polish

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,16 @@ class Blog(models.Model):
     #body  
     body= models.TextField()
 
+    #changing the title of the block object to the title of the blog post
+    def __str__(self):
+        return self.title
 
+    #post previews
+    def summary(self):
+        return self.body[:50]
+    #to make the time and date looks better
+    def pub_date_pretty(self):
+        return self.pub_date.strftime( '%b %e %Y' )
 
 # add blog app to settings
 # create migrattion
--- portfolios/urls.py
+++ portfolios/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import urls
 from django.contrib import admin
 from django.urls import path, include
 from   django.conf import settings
@@ -23,4 +24,5 @@ from .import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
+    path('blog/', include ('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
